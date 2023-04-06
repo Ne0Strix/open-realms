5ceb212ae6388db313d22743282cfa8a7d8d8b5f
soundossaidi <saidi.soundos2002@gmail.com>
2023-04-06 16:44:55 +0200

    packege name

--- app/src/main/java/com/example/open_realms/model/GainCoins.java
+++
@@ -1,4 +0,0 @@
-/* Licensed under GNU GPL v3.0 (C) 2023 */
-package com.example.open_realms.model;
-
-public class GainCoins {}
---
+++ app/src/test/java/at/vunfer/openrealms/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+/* Licensed under GNU GPL v3.0 (C) 2023 */
+package at.vunfer.openrealms;
+
+import static org.junit.Assert.*;
+
+import org.testng.annotations.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
