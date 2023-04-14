12f4f1a04d0fd9fb89b485472c7711a76af61fdb
Julia Lomonosova <julie.lomonosova283@gmail.com>
2023-04-14 19:44:35 +0200

    Changes

---
+++ app/src/main/java/at/vunfer/openrealms/GameActivity.java
@@ -0,0 +1,35 @@
+package at.vunfer.openrealms.Client_Server.app.src.main.java.com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class GameActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        Log.d("Debug", "Starting game activity...");
+
+        // this hostname is used for the Emulator!
+        ClientThread clientThread = new ClientThread(8040, "10.0.2.2");
+        clientThread.start();
+
+        TextView textView = findViewById(R.id.exampleView);
+
+        Button btn = findViewById(R.id.exampleBtn);
+        btn.setOnClickListener(view -> {
+            clientThread.sendStringToServer();
+            textView.setText(clientThread.getFromServer());
+        });
+
+    }
+
+}
