5126cabdbfcae7e5ceb48f38fb693cde5f2c89f6
Benjamin Dippold <dippold.benjamin@gmail.com>
2023-04-15 15:47:03 +0200

    Fixed format violations by manually executing spotlessApply

--- app/src/androidTest/java/at/vunfer/openrealms/model/DeckGeneratorTest.java
+++ app/src/androidTest/java/at/vunfer/openrealms/model/DeckGeneratorTest.java
@@ -1,32 +1,38 @@
+/* Licensed under GNU GPL v3.0 (C) 2023 */
 package at.vunfer.openrealms.model;
 
 import static org.junit.Assert.assertEquals;
 
+import at.vunfer.openrealms.model.effects.CoinEffect;
+import at.vunfer.openrealms.model.effects.DamageEffect;
+import at.vunfer.openrealms.model.effects.HealingEffect;
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-import java.io.ByteArrayInputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import at.vunfer.openrealms.model.effects.CoinEffect;
-import at.vunfer.openrealms.model.effects.DamageEffect;
-import at.vunfer.openrealms.model.effects.HealingEffect;
-
 public class DeckGeneratorTest {
 
     @Test
     public void testGenerateDeckFromXml() {
-        String xmlToParse = "<deck><card amount=\"3\"><name>testName</name><cost>5</cost><ability><amount>5</amount><type>coin</type></ability></card><card amount=\"1\"><name>otherTestName</name><cost>16</cost><ability><amount>2</amount><type>attack</type></ability><ability><amount>5</amount><type>heal</type></ability></card></deck>";
+        String xmlToParse =
+                "<deck><card"
+                    + " amount=\"3\"><name>testName</name><cost>5</cost><ability><amount>5</amount><type>coin</type></ability></card><card"
+                    + " amount=\"1\"><name>otherTestName</name><cost>16</cost><ability><amount>2</amount><type>attack</type></ability><ability><amount>5</amount><type>heal</type></ability></card></deck>";
         XmlPullParser xml = getXmlFromString(xmlToParse);
 
         Deck<Card> expectedDeck = new Deck<>();
         expectedDeck.add(new Card("testName", 5, new ArrayList<>(List.of(new CoinEffect(5)))));
         expectedDeck.add(new Card("testName", 5, new ArrayList<>(List.of(new CoinEffect(5)))));
         expectedDeck.add(new Card("testName", 5, new ArrayList<>(List.of(new CoinEffect(5)))));
-        expectedDeck.add(new Card("otherTestName", 16, new ArrayList<>(List.of(new DamageEffect(2), new HealingEffect(5)))));
+        expectedDeck.add(
+                new Card(
+                        "otherTestName",
+                        16,
+                        new ArrayList<>(List.of(new DamageEffect(2), new HealingEffect(5)))));
 
         Deck<Card> deck = DeckGenerator.generateDeckFromXml(xml);
         assertEquals(expectedDeck, deck);
