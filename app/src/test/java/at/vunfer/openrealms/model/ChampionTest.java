eefa3a0f41b8e6c8807a4e7ac8b87dc5eaf6e22a
Manuel Hoi <manuel2.hoi@gmail.com>
2023-04-18 19:37:10 +0200

    Correction to commit 2614951c
    Somehow the actual PlayAreaTest did not make it into the commit, so here it is
    
    relates to #7 and #11

--- app/src/test/java/at/vunfer/openrealms/model/ChampionTest.java
+++ app/src/test/java/at/vunfer/openrealms/model/ChampionTest.java
@@ -1,39 +1,45 @@
+/* Licensed under GNU GPL v3.0 (C) 2023 */
 package at.vunfer.openrealms.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
 import java.util.List;
+import org.junit.jupiter.api.Test;
 
 public class ChampionTest {
 
     @Test
     public void testConstructorValid() {
         // Test valid construction
-        List<Effect> effects = List.of(new DamageEffect(1), new HealingEffect(1), new CoinEffect(1));
+        List<Effect> effects =
+                List.of(new DamageEffect(1), new HealingEffect(1), new CoinEffect(1));
         Champion champion = new Champion("Test Champion", 3, effects);
         assertEquals("Test Champion", champion.getName());
         assertEquals(3, champion.getCost());
         assertEquals(effects, champion.getEffects());
-
     }
 
     @Test
     public void testConstructorInvalid() {
         // Test invalid construction
-        List<Effect> effects = List.of(new DamageEffect(1), new HealingEffect(1), new CoinEffect(1));
+        List<Effect> effects =
+                List.of(new DamageEffect(1), new HealingEffect(1), new CoinEffect(1));
         assertThrows(IllegalArgumentException.class, () -> new Champion("", 3, effects));
-        assertThrows(IllegalArgumentException.class, () -> new Champion("Test Champion", -3, effects));
+        assertThrows(
+                IllegalArgumentException.class, () -> new Champion("Test Champion", -3, effects));
         assertThrows(IllegalArgumentException.class, () -> new Champion("Test Champion", 3, null));
-        assertThrows(IllegalArgumentException.class, () -> new Champion("Test Champion", 3, List.of()));
+        assertThrows(
+                IllegalArgumentException.class, () -> new Champion("Test Champion", 3, List.of()));
     }
 
     @Test
     public void testApplyEffects() {
         // Set up play area and champion with effects
         PlayArea playArea = new PlayArea(20, new PlayerCards());
-        Champion champion = new Champion("Test Champion", 3, List.of(new DamageEffect(1), new HealingEffect(1)));
+        Champion champion =
+                new Champion(
+                        "Test Champion", 3, List.of(new DamageEffect(1), new HealingEffect(1)));
 
         // Apply effects to play area
         champion.applyEffects(playArea);
@@ -45,7 +51,11 @@ public class ChampionTest {
 
     @Test
     public void testToString() {
-        Champion champion = new Champion("Test Champion", 3, List.of(new DamageEffect(1), new HealingEffect(1), new CoinEffect(1)));
+        Champion champion =
+                new Champion(
+                        "Test Champion",
+                        3,
+                        List.of(new DamageEffect(1), new HealingEffect(1), new CoinEffect(1)));
         assertEquals("Card{name='Test Champion', cost=3}", champion.toString());
     }
 }
