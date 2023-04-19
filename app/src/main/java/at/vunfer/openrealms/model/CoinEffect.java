0e944093f3589179b14c080a3ca89c9225858ec3
soundossaidi <saidi.soundos2002@gmail.com>
2023-04-19 13:24:44 +0200

    Revert "Implement UI-components for MarketView and PlayerArea view (#22)"
    This reverts commit fd5f4af4e7df0e41063fda350820d6a87531f0f2.

--- app/src/main/java/at/vunfer/openrealms/model/CoinEffect.java
+++ app/src/main/java/at/vunfer/openrealms/model/CoinEffect.java
@@ -13,9 +13,4 @@ public class CoinEffect implements Effect {
     public void applyEffect(PlayArea visitor) {
         visitor.visitCoin(coin);
     }
-
-    @Override
-    public String getDescription() {
-        return "Gain " + coin + " coins.";
-    }
 }
--- app/src/main/java/at/vunfer/openrealms/presenter/View.java
+++
@@ -1,9 +0,0 @@
-/* Licensed under GNU GPL v3.0 (C) 2023 */
-package at.vunfer.openrealms.presenter;
-
-import at.vunfer.openrealms.model.Card;
-import java.util.List;
-
-public interface View {
-    void displayMarket(List<Card> market);
-}
