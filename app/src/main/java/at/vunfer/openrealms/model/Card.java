fd5f4af4e7df0e41063fda350820d6a87531f0f2
soundossaidi <saidi.soundos2002@gmail.com>
2023-04-19 13:20:07 +0200

    Implement UI-components for MarketView and PlayerArea view (#22)
    Add custom UI-components to display the market area and publicly visible player area in the game activity (#23). Note: implementation is still rough and needs further refinement. This feature fulfills the user story of allowing players to view the market area during gameplay.

--- app/src/main/java/at/vunfer/openrealms/model/Card.java
+++ app/src/main/java/at/vunfer/openrealms/model/Card.java
@@ -1,6 +1,10 @@
 /* Licensed under GNU GPL v3.0 (C) 2023 */
 package at.vunfer.openrealms.model;
 
+import android.content.Context;
+import android.widget.ImageView;
+import at.vunfer.openrealms.R;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Card {
@@ -9,7 +13,21 @@ public class Card {
     private int cost;
     private List<Effect> effects;
 
-    public Card(String name, int cost, List<Effect> effects) throws IllegalArgumentException {
+    private final int imageResource;
+    private final String description;
+    private ImageView cardImage;
+
+    public Card(Context context) {
+        this.name = "Empty Card";
+        this.cost = 0;
+        this.effects = new ArrayList<>();
+        this.imageResource = R.drawable.emptycards;
+        this.cardImage = new ImageView(context);
+        this.description = "";
+    }
+
+    public Card(String name, int cost, List<Effect> effects, String description)
+            throws IllegalArgumentException {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Name must not be null or empty");
         } else if (cost < 0) {
@@ -20,6 +38,9 @@ public class Card {
         this.name = name;
         this.cost = cost;
         this.effects = effects;
+
+        this.imageResource = R.drawable.emptycards;
+        this.description = description;
     }
 
     public String getName() {
@@ -43,5 +64,21 @@ public class Card {
     @Override
     public String toString() {
         return "Card{" + "name='" + name + '\'' + ", cost=" + cost + '}';
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public ImageView getCardImage() {
+        return cardImage;
+    }
+
+    public void setCardImage(ImageView cardImage) {
+        this.cardImage = cardImage;
     }
 }
