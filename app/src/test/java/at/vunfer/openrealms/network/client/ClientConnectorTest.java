5ad13b8b35a5c42da8fc9396ad74ba2aa2cfd6d5
Armin Lachini <arminla@edu.aau.at>
2023-05-03 21:46:18 +0200

    spotlessApply

--- app/src/test/java/at/vunfer/openrealms/network/client/ClientConnectorTest.java
+++ app/src/test/java/at/vunfer/openrealms/network/client/ClientConnectorTest.java
@@ -1,20 +1,17 @@
+/* Licensed under GNU GPL v3.0 (C) 2023 */
 package at.vunfer.openrealms.network.client;
 
-import at.vunfer.openrealms.UIUpdateListener;
-import at.vunfer.openrealms.network.Message;
-import at.vunfer.openrealms.network.MessageType;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
+import at.vunfer.openrealms.UIUpdateListener;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ClientConnectorTest {
 
@@ -33,6 +30,8 @@ public class ClientConnectorTest {
     @Test
     public void setConnectionTargetTest() throws IOException {
         clientConnector.setConnectionTarget("192.168.1.100", 3000);
-        assertEquals(clientConnector.getConnectionTarget(), new InetSocketAddress("192.168.1.100", 3000));
+        assertEquals(
+                clientConnector.getConnectionTarget(),
+                new InetSocketAddress("192.168.1.100", 3000));
     }
 }
