6ecd3c2789b753a36a11dfe40a277c1f52a6806b
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-05-08 21:19:20 +0200

    Add necessary methods

--- app/src/main/java/at/vunfer/openrealms/UIUpdateListener.java
+++ app/src/main/java/at/vunfer/openrealms/UIUpdateListener.java
@@ -1,8 +1,15 @@
 /* Licensed under GNU GPL v3.0 (C) 2023 */
 package at.vunfer.openrealms;
 
+import java.util.List;
+
+import at.vunfer.openrealms.model.Card;
 import at.vunfer.openrealms.network.Message;
 
 public interface UIUpdateListener {
     void updateUI(Message message);
+    void addCardToPlayArea(Card card);
+    void removeCardFromPlayArea(Card card);
+    String displayOptions(List<String> options);
+    //void updatePlayerStats(PlayerStats stats);
 }
