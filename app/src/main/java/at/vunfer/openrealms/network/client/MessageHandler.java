8918acf5597c6ea6ea1b293ed0fd49f7cd1bf763
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-05-08 21:36:04 +0200

    Refactor MessageHandler class to use UIUpdateListener interface and handle message data correctly

--- app/src/main/java/at/vunfer/openrealms/network/client/MessageHandler.java
+++ app/src/main/java/at/vunfer/openrealms/network/client/MessageHandler.java
@@ -1,34 +1,49 @@
 /* Licensed under GNU GPL v3.0 (C) 2023 */
 package at.vunfer.openrealms.network.client;
 
+import java.util.List;
+
+import at.vunfer.openrealms.UIUpdateListener;
+import at.vunfer.openrealms.model.Card;
+import at.vunfer.openrealms.network.DataKey;
 import at.vunfer.openrealms.network.Message;
 
 public class MessageHandler {
+    private UIUpdateListener uiUpdateListener;
 
     public boolean handleMessage(Message message) {
         switch (message.getType()) {
             case ADD_CARD:
                 handleAddCardMessage(message);
+                break;
             case REMOVE_CARD:
                 handleRemoveCardMessage(message);
+                break;
             case CHOOSE_OPTION:
                 handleChooseOptionMessage(message);
+                break;
             case UPDATE_PLAYER_STATS:
                 handleUpdatePlayerStatsMessage(message);
+                break;
             default:
                 return false;
         }
+        return true;
     }
     private void handleAddCardMessage(Message message) {
-        // TODO instructions for UI
+        Card card = (Card) message.getData(DataKey.CARD_ID);
+        uiUpdateListener.addCardToPlayArea(card);
     }
     private void handleRemoveCardMessage(Message message) {
-        // TODO instructions for UI
+        Card card = (Card) message.getData(DataKey.CARD_ID);
+        uiUpdateListener.removeCardFromPlayArea(card);
     }
     private void handleChooseOptionMessage(Message message) {
-        // TODO instructions for UI
+        List<String> options = (List<String>) message.getData(DataKey.OPTIONS);
+        String selectedOption = uiUpdateListener.displayOptions(options);
     }
     private void handleUpdatePlayerStatsMessage(Message message) {
-        // TODO instructions for UI
+        //PlayerStats stats = (PlayerStats) message.getData();
+        // uiUpdateListener.updatePlayerStats(stats); - not implemented
     }
 }
