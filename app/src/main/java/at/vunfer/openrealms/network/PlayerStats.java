bbdc992c6aff8675a11e24c29cfb4e555c2df384
Manuel Hoi <manuel2.hoi@gmail.com>
2023-05-08 17:37:02 +0200

    Added PlayerStats Class for encapsulation
    Since it is necessary to notify all clients about changes in their displayed stats and
    Turn Values, I decided to make a DataClass that will hold this data when sending it
    Over the network.

---
+++ app/src/main/java/at/vunfer/openrealms/network/PlayerStats.java
@@ -0,0 +1,59 @@
+/* Licensed under GNU GPL v3.0 (C) 2023 */
+package at.vunfer.openrealms.network;
+
+public class PlayerStats {
+    private String playerName;
+    private int playerHealth;
+    private int turnDamage;
+    private int turnHealing;
+    private int turnCoin;
+
+    public PlayerStats(String playerName, int playerHealth, int turnDamage, int turnHealing, int turnCoin) {
+        this.playerName = playerName;
+        this.playerHealth = playerHealth;
+        this.turnDamage = turnDamage;
+        this.turnHealing = turnHealing;
+        this.turnCoin = turnCoin;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getPlayerHealth() {
+        return playerHealth;
+    }
+
+    public void setPlayerHealth(int playerHealth) {
+        this.playerHealth = playerHealth;
+    }
+
+    public int getTurnDamage() {
+        return turnDamage;
+    }
+
+    public void setTurnDamage(int turnDamage) {
+        this.turnDamage = turnDamage;
+    }
+
+    public int getTurnHealing() {
+        return turnHealing;
+    }
+
+    public void setTurnHealing(int turnHealing) {
+        this.turnHealing = turnHealing;
+    }
+
+    public int getTurnCoin() {
+        return turnCoin;
+    }
+
+    public void setTurnCoin(int turnCoin) {
+        this.turnCoin = turnCoin;
+    }
+
+}
