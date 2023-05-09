1894cb90875b8f594d42e69b97482a2f72bc6add
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-05-09 15:13:12 +0200

    Applied Spotless

--- app/src/main/java/at/vunfer/openrealms/UIUpdateListener.java
+++ app/src/main/java/at/vunfer/openrealms/UIUpdateListener.java
@@ -1,15 +1,18 @@
 /* Licensed under GNU GPL v3.0 (C) 2023 */
 package at.vunfer.openrealms;
 
-import java.util.List;
-
 import at.vunfer.openrealms.model.Card;
 import at.vunfer.openrealms.network.Message;
+import java.util.List;
 
 public interface UIUpdateListener {
     void updateUI(Message message);
+
     void addCardToPlayArea(Card card);
+
     void removeCardFromPlayArea(Card card);
+
     String displayOptions(List<String> options);
-    //void updatePlayerStats(PlayerStats stats);
+    // void updatePlayerStats(PlayerStats stats);
+
 }
