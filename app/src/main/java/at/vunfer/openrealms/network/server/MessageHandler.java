05f44bc1c69f912f465f3cdf53098a2026e7df36
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-05-09 09:13:02 +0200

    Refactor MessageHandler class with handleMessage method for handling TOUCHED, CHOICE, and END_TURN message types

--- app/src/main/java/at/vunfer/openrealms/network/server/MessageHandler.java
+++ app/src/main/java/at/vunfer/openrealms/network/server/MessageHandler.java
@@ -1,18 +1,29 @@
 /* Licensed under GNU GPL v3.0 (C) 2023 */
 package at.vunfer.openrealms.network.server;
 
+import java.util.Map;
+
+import at.vunfer.openrealms.network.DataKey;
 import at.vunfer.openrealms.network.Message;
 
 public class MessageHandler {
     public boolean handleMessage(Message message) {
         switch (message.getType()) {
             case TOUCHED:
-                // TODO instructions for backend
+                System.out.println("Received TOUCHED message.");
+                return true;
             case CHOICE:
-                // TODO instructions for backend
+                Object rawData = message.getData(DataKey.OPTIONS);
+                if (rawData instanceof Map) {
+                    Map<String, Object> data = (Map<String, Object>) rawData;
+                    System.out.println("Received CHOICE message with option: " + data.get("option"));
+                }
+                return true;
             case END_TURN:
-                // TODO instructions for backend
+                System.out.println("Received END_TURN message. Ending turn...");
+                return true;
             default:
+                System.out.println("Received unknown message type: " + message.getType());
                 return false;
         }
     }
