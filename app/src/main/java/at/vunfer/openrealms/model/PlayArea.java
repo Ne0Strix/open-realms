8cd036acc02356bac07bf4428235bbdb3a170cb3
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-05-19 17:22:27 +0200

    Add phone orientation detection in isPhoneTurnedOver()
    This commit adds functionality to the isPhoneTurnedOver() method in the PlayArea class. The method now uses the accelerometer sensor to detect the orientation of the phone. It calculates the magnitude of the accelerometer values and compares it with the gravity constant to determine if the phone is turned over or upside down. Depending on the orientation, the method can perform specific actions or return true/false accordingly.
    
    This change enhances the cheat function in the game by enabling additional conditions for activating the cheat mode based on the phone's orientation.

--- app/src/main/java/at/vunfer/openrealms/model/PlayArea.java
+++ app/src/main/java/at/vunfer/openrealms/model/PlayArea.java
@@ -1,6 +1,12 @@
 /* Licensed under GNU GPL v3.0 (C) 2023 */
 package at.vunfer.openrealms.model;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
 import java.util.List;
 
 import at.vunfer.openrealms.network.DataKey;
@@ -10,26 +16,29 @@ import at.vunfer.openrealms.network.Message;
  * Represents the play area in the game. Stores the current state of the game and provides methods
  * for manipulating it.
  */
-public class PlayArea {
+public class PlayArea extends Thread {
     private int health;
     private int turnDamage;
     private int turnHealing;
     private int turnCoins;
 
     private Market market;
-    private Deck<Card> playedCards;
-    private Deck<Card> playedChampions;
-    private PlayerCards playerCards;
+    private final Deck<Card> playedCards;
+    private final Deck<Card> playedChampions;
+    private final PlayerCards playerCards;
+    private final Context context;
+
+    private boolean cheat;
 
     /**
      * Constructs a new PlayArea object with the specified health and player cards. Initializes the
      * turn damage, healing, and coins to 0, and initializes the played cards and played champions
      * decks.
      *
-     * @param health The health of the player.
+     * @param health      The health of the player.
      * @param playerCards The player's cards.
      */
-    public PlayArea(int health, PlayerCards playerCards) {
+    public PlayArea(int health, PlayerCards playerCards, Context context) {
 
         this.health = health;
         this.turnDamage = 0;
@@ -38,6 +47,8 @@ public class PlayArea {
         this.playedCards = new Deck<>();
         this.playedChampions = new Deck<>();
         this.playerCards = playerCards;
+        this.market = Market.getInstance();
+        this.context = context;
         this.market = Market.getInstance();
     }
 
@@ -145,7 +156,9 @@ public class PlayArea {
     //        return null;
     //    }
 
-    /** Resets the turn damage, healing, and coins to 0. */
+    /**
+     * Resets the turn damage, healing, and coins to 0.
+     */
     public void resetTurnPool() {
         this.turnDamage = 0;
         this.turnHealing = 0;
@@ -206,13 +219,56 @@ public class PlayArea {
                 throw new IllegalArgumentException("Not enough coins this turn");
             }
             if ((Boolean) message.getData(DataKey.CHEAT_ACTIVATE)) {
-                turnCoins += cardToBuy.getCost();
+                // Check if phone is turned over or upside down (cheating condition)
+                if (this.cheat) {
+                    turnCoins += cardToBuy.getCost();
+                }
+                turnCoins -= cardToBuy.getCost();
+                market.purchase(cardToBuy);
+                playerCards.addBoughtCard(cardToBuy);
+            } else {
+                throw new IllegalArgumentException("Card does not exist");
             }
-            turnCoins -= cardToBuy.getCost();
-            market.purchase(cardToBuy);
-            playerCards.addBoughtCard(cardToBuy);
-        } else {
-            throw new IllegalArgumentException("Card does not exist");
+        }
+    }
+
+    public boolean isPhoneTurnedOver() {
+        // Get the sensor service
+        SensorManager sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+
+        // Check if the device has an accelerometer sensor
+        if (sensorManager != null && sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            SensorEventListener sensorEventListener = new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    float x = event.values[0];
+                    float y = event.values[1];
+                    float z = event.values[2];
+
+                    // Calculate the device orientation
+                    double magnitude = Math.sqrt(x * x + y * y + z * z);
+                    double gravity = SensorManager.GRAVITY_EARTH;
+                    double delta = Math.abs(gravity - magnitude);
+
+                    // Check if the phone is turned over or upside down
+                    if (delta > 2.0) {
+                    } else {
+                    }
+                }
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                }
+            };
+            Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        return false;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            this.cheat = this.isPhoneTurnedOver();
         }
     }
 }
