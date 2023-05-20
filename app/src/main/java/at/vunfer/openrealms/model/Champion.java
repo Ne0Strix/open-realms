40d7808d102df1898ebd5db0f26e624e34c329cf
Manuel Hoi <manuel2.hoi@gmail.com>
2023-05-20 23:24:24 +0200

    implementation of Champion-class
    Up until now, champions were not part of the game. Since this will
    change in sprint 3, I now implemented the required attributes and
    methods the class needs in order to function correctly in accordance with
    the game rules. Champions extend Cards, but they now have their own
    health and can be killed. The also can be expended in the players turn,
    which would reapply their effects (handled by PlayArea).

--- app/src/main/java/at/vunfer/openrealms/model/Champion.java
+++ app/src/main/java/at/vunfer/openrealms/model/Champion.java
@@ -4,8 +4,45 @@ package at.vunfer.openrealms.model;
 import java.util.List;
 
 public class Champion extends Card {
-    public Champion(String name, int cost, CardType type, List<Effect> effects)
+
+    private final boolean isGuard;
+    private final int health;
+    private boolean isExpended;
+
+    public Champion(
+            String name, int cost, CardType type, List<Effect> effects, boolean isGuard, int health)
             throws IllegalArgumentException {
         super(name, cost, type, effects);
+        this.isGuard = isGuard;
+        this.health = health;
+        this.isExpended = false;
+    }
+
+    public boolean isKilled(int damage) {
+        return health <= damage;
+    }
+
+    public boolean isGuard() {
+        return isGuard;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public boolean isExpended() {
+        return isExpended;
+    }
+
+    public boolean expend() {
+        if (isExpended) {
+            return false;
+        }
+        isExpended = true;
+        return true;
+    }
+
+    public void reset() {
+        isExpended = false;
     }
 }
