4eadff72ef7d61fc12f9224dfa4f4cd9a2a8f5a2
sosaidi <122746499+sosaidi@users.noreply.github.com>
2023-05-21 23:07:09 +0200

    Merge branch 'feature/62-cheating' into develop
    Resolve merge conflicts and incorporate changes from the 'feature/62-cheating' branch into the 'develop' branch.

--- app/src/main/java/at/vunfer/openrealms/network/client/ClientConnector.java
+++ app/src/main/java/at/vunfer/openrealms/network/client/ClientConnector.java
@@ -25,7 +25,7 @@ public class ClientConnector extends Thread {
         this.uiUpdater = uiUpdater;
         try {
             // Connect to the server
-            socket = new Socket(serverAddress, serverPort);
+            //socket = new Socket(serverAddress, serverPort);
             outputStream = new ObjectOutputStream(socket.getOutputStream());
             inputStream = new ObjectInputStream(socket.getInputStream());
             new Thread(this::listenForMessages).start();
