9f7f22ef7b44181b17cec4dc084c79806d5fc4f9
Benjamin-Dippold <dippold.benjamin@gmail.com>
2023-06-21 14:25:39 +0200

    Removed overconfident Log in Model-class

--- app/src/main/java/at/vunfer/openrealms/network/server/ServerMessageHandler.java
+++ app/src/main/java/at/vunfer/openrealms/network/server/ServerMessageHandler.java
@@ -32,7 +32,6 @@ public class ServerMessageHandler implements IHandleMessage {
     }
 
     public void handleMessage(Message message) throws IOException {
-        Log.i(TAG, "Received: " + message);
         ensureServerThreadInitialized();
 
         GameSession gameSession = serverThread.getGameSession();
